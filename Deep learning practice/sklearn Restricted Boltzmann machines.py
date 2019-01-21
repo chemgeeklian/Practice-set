92d26b72e750408caee1d41442eda45cecaee964
ChemLian <xinran.lian@gmail.com>
2019-01-21 14:18:27 -0600

    update

---
+++ Deep learning practice/sklearn Restricted Boltzmann machines.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+2.9.1. Restricted Boltzmann machines
+
+https://scikit-learn.org/stable/modules/neural_networks_unsupervised.html
+scikit-learn only provides BernoulliRBM, which assumes the inputs are either 
+binary values or values between 0 and 1
+
+@author: Administrator
+"""
+
