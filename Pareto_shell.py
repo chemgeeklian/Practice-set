bad32fe15c1b52a150c15661e6ac7b9e61900614
Xinran Lian <xinran.lian@gmail.com>
2019-08-23 10:16:14 -0500

    add shell function

--- Pareto_shell.py
+++ Pareto_shell.py
@@ -6,6 +6,8 @@ https://pythonhealthcare.org/tag/pareto-front/
 
 Slightly modify the codes to make it able to run in numba for faster computation.
 
+And repeating obtaining pareto front layers to get a pareto shell...
+
 @author: xinran
 """
 
@@ -13,9 +15,12 @@ import numpy as np
 from numba import jit
 import matplotlib.pyplot as plt
 
+n_points = 1000
+n_iter = 5
+
 mu, sigma = 0, 0.1 # mean and standard deviation
 np.random.seed(23)
-s = np.random.normal(mu, sigma, [600000,2])
+s = np.random.normal(mu, sigma, [n_points,2]) # Generate a demo Gaussian distribution
 
 @jit(nopython = True)
 def identify_pareto(scores):
@@ -41,10 +46,22 @@ def identify_pareto(scores):
     # Return ids of scenarios on pareto front
     return population_ids[np.where(pareto_front==1)]
 
+def pareto_shell(score, n_iter):
+    slices = []
+    original_index = np.arange(len(score))
+    score_with_index = np.concatenate([score, original_index[:,None]],axis=1)
+    for iteration in range(n_iter):
+        pareto_layer = identify_pareto(score_with_index[:,:-1])
+        slices.append(score_with_index[pareto_layer,-1])
+        score_with_index = np.delete(score_with_index,pareto_layer,0)
+    #slices = np.array(slices)
+    return(np.concatenate(slices).ravel())
+
 x = s[:, 0]
 y = s[:, 1]
 
-a = identify_pareto(s)
+#a = identify_pareto(s)
+a = pareto_shell(s, n_iter).astype(int)
 
 # Scatter the points with the ones on the pareto frontier.
 plt.figure(figsize = [5,5])
